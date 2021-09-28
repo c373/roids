0e52a6ed3d9b875b12ccf2e40cc1c55490f55a47
carlos cruz <76661778+c373@users.noreply.github.com>
2021-09-27 20:35:57 -0700

    refectored

--- asteroids.lua
+++ asteroids.lua
@@ -1,40 +1,16 @@
-function rotate( x, y, a )
-
-	local c = math.cos( a )
-	local s = math.sin( a )
-
-	return c*x - s*y, s*x + c*y
-
-end
-
-function vertexListToVertexColorList( vertices )
-
-	local vertexColorList = {}
-
-	for i = 1, #vertices, 2 do
-		table.insert( vertexColorList, { vertices[i], vertices[i + 1], 0, 0, 1, 1, 1, 1 } )
-	end
-
-	return vertexColorList
-
-end
-
 function newAsteroid( radiusRange )
 
 	local angleRange = { min = 25, max = 50 }
 
 	local angleEaten = 0
 
-	--local vertices = { 0, 0 }
 	local vertices = { }
 	
 	while angleEaten < 360 do
 
 		local radius = love.math.random( radiusRange.min, radiusRange.max )
-		--local radius = 100
 
 		local angle = love.math.random( angleRange.min, angleRange.max )
-		--local angle = 30
 
 		if angleEaten + angle > 360 then
 			angle = 360 - angleEaten
@@ -53,10 +29,6 @@ function newAsteroid( radiusRange )
 
 	end
 
-	--table.insert( vertices, vertices[3] )
-	--table.insert( vertices, vertices[4] )  
-
-	--return vertexListToVertexColorList( vertices )
 	return vertices
 
 end
--- main.lua
+++ main.lua
@@ -21,6 +21,27 @@ function createAsteroid()
 
 end
 
+function rotate( x, y, a )
+
+	local c = math.cos( a )
+	local s = math.sin( a )
+
+	return c*x - s*y, s*x + c*y
+
+end
+
+function vertexListToVertexColorList( vertices )
+
+	local vertexColorList = {}
+
+	for i = 1, #vertices, 2 do
+		table.insert( vertexColorList, { vertices[i], vertices[i + 1], 0, 0, 1, 1, 1, 1 } )
+	end
+
+	return vertexColorList
+
+end
+
 function love.load()
 
 	love.frame = 0
