49b6a9f7e21de84f42880b89cc34126d4cf453ac
carlos cruz <76661778+c373@users.noreply.github.com>
2022-01-31 23:17:14 -0800

    the shader is done papi!
    I just need to optimize/organize the code

--- screenwrap.fs
+++ screenwrap.fs
@@ -3,38 +3,107 @@ uniform float height;
 uniform float offsetWidth;
 uniform float offsetHeight;
 
+bool check( vec4 sample )
+{
+	if ( sample.r != 0 && sample.g != 0 && sample.b != 0 && sample.a != 0 )
+		return true;
+	else
+		return false;
+}
+
 vec4 effect( vec4 color, Image tex, vec2 uv, vec2 px )
 {
 	vec4 orig = Texel( tex, uv );
 	vec4 sample = orig;
 
-	//left wrapzone
-	if ( uv.x < offsetWidth * 2 && uv.y > offsetWidth * 2 && 1 - uv.y > offsetWidth * 2 )
+	//topleft
+	if ( uv.x < offsetWidth * 2 && uv.y < offsetHeight * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x + width, uv.y + height ) );
+		if( check( sample ) )
+			return color * sample;
 		sample = Texel( tex, vec2( uv.x + width, uv.y ) );
-
-	//right wrapzone
-	if ( 1 - uv.x < offsetWidth * 2 && uv.y > offsetWidth * 2 && 1 - uv.y > offsetWidth * 2 )
-		sample = Texel( tex, vec2( uv.x - width, uv.y ) );
-
-	//top wrapzone
-	if ( uv.y < offsetHeight * 2 && uv.x > offsetHeight * 2 && 1 - uv.x > offsetHeight * 2 )
+		if( check( sample ) )
+			return color * sample;
 		sample = Texel( tex, vec2( uv.x, uv.y + height ) );
-	
-	//bottom wrapzone
-	if ( 1 - uv.y < offsetHeight * 2 && uv.x > offsetHeight * 2 && 1 - uv.x > offsetHeight * 2 )
+		if( check( sample ) )
+			return color * sample;
+	}
+
+	//topright
+	if ( 1 - uv.x < offsetWidth * 2 && uv.y < offsetHeight * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x - width, uv.y + height ) );
+		if( check( sample ) )
+			return color * sample;
+		sample = Texel( tex, vec2( uv.x - width, uv.y ) );
+		if( check( sample ) )
+			return color * sample;
+		sample = Texel( tex, vec2( uv.x, uv.y + height ) );
+		if( check( sample ) )
+			return color * sample;
+	}
+
+	//bottomleft
+	if ( uv.x < offsetWidth * 2 && 1 - uv.y < offsetHeight * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x + width, uv.y - height ) );
+		if( check( sample ) )
+			return color * sample;
+		sample = Texel( tex, vec2( uv.x + width, uv.y ) );
+		if( check( sample ) )
+			return color * sample;
 		sample = Texel( tex, vec2( uv.x, uv.y - height ) );
+		if( check( sample ) )
+			return color * sample;
+	}
 
+	//bottomright
+	if ( 1 - uv.x < offsetWidth * 2 && 1 - uv.y < offsetHeight * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x - width, uv.y - height ) );
+		if( check( sample ) )
+			return color * sample;
+		sample = Texel( tex, vec2( uv.x - width, uv.y ) );
+		if( check( sample ) )
+			return color * sample;
+		sample = Texel( tex, vec2( uv.x, uv.y - height ) );
+		if( check( sample ) )
+			return color * sample;
+	}
 
-	if (sample.r != 0 && sample.g != 0 && sample.b != 0 && sample.a != 0 )
-		return color * sample;
-	else
-		return color * orig;
+	//left 
+	if ( uv.x < offsetWidth * 2 && uv.y > offsetHeight * 2 && 1 - uv.y > offsetHeight * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x + width, uv.y ) );
+		if( check( sample ) )
+			return color * sample;
+	}
+
+	//right 
+	if ( 1 - uv.x < offsetWidth * 2 && uv.y > offsetHeight * 2 && 1 - uv.y > offsetHeight * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x - width, uv.y ) );
+		if( check( sample ) )
+			return color * sample;
+	}
+
+	//top 
+	if ( uv.y < offsetHeight * 2 && uv.x > offsetWidth * 2 && 1 - uv.x > offsetWidth * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x, uv.y + height ) );
+		if( check( sample ) )
+			return color * sample;
+	}
+	
+	//bottom 
+	if ( 1 - uv.y < offsetHeight * 2 && uv.x > offsetWidth * 2 && 1 - uv.x > offsetWidth * 2 )
+	{
+		sample = Texel( tex, vec2( uv.x, uv.y - height ) );
+		if( check( sample ) )
+			return color * sample;
+	}
+
+	return color * orig;	
 }
 
-bool check( vec4 sample )
-{
-	if (sample.r != 0 && sample.g != 0 && sample.b != 0 && sample.a != 0 )
-		return false;
-	else
-		return true;
-}
