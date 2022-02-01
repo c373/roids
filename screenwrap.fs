30def3e096bc500cbf9f160fe1a8f2e73c826c12
carlos cruz <76661778+c373@users.noreply.github.com>
2022-01-31 21:16:28 -0800

    got the cardinal direction wraps going

--- screenwrap.fs
+++ screenwrap.fs
@@ -6,23 +6,24 @@ uniform float offsetHeight;
 vec4 effect( vec4 color, Image tex, vec2 uv, vec2 px )
 {
 	vec4 orig = Texel( tex, uv );
-	vec4 sample;
+	vec4 sample = orig;
 
 	//left wrapzone
-	if ( uv.x < offsetWidth * 2  )
+	if ( uv.x < offsetWidth * 2 && uv.y > offsetWidth * 2 && 1 - uv.y > offsetWidth * 2 )
 		sample = Texel( tex, vec2( uv.x + width, uv.y ) );
 
 	//right wrapzone
-	if ( 1 - uv.x < offsetWidth * 2  )
+	if ( 1 - uv.x < offsetWidth * 2 && uv.y > offsetWidth * 2 && 1 - uv.y > offsetWidth * 2 )
 		sample = Texel( tex, vec2( uv.x - width, uv.y ) );
 
 	//top wrapzone
-	if ( uv.y < offsetHeight * 2  )
+	if ( uv.y < offsetHeight * 2 && uv.x > offsetHeight * 2 && 1 - uv.x > offsetHeight * 2 )
 		sample = Texel( tex, vec2( uv.x, uv.y + height ) );
 	
 	//bottom wrapzone
-	if ( 1 - uv.y < offsetHeight * 2  )
+	if ( 1 - uv.y < offsetHeight * 2 && uv.x > offsetHeight * 2 && 1 - uv.x > offsetHeight * 2 )
 		sample = Texel( tex, vec2( uv.x, uv.y - height ) );
+
 
 	if (sample.r != 0 && sample.g != 0 && sample.b != 0 && sample.a != 0 )
 		return color * sample;
