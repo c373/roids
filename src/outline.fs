27b54150a04c8b0d3833545f5188646241ac8d4b
c373 <76661778+c373@users.noreply.github.com>
2022-05-17 17:12:10 -0700

    add diagonal processing for outline shader

--- src/outline.fs
+++ src/outline.fs
@@ -1,13 +1,18 @@
 vec4 resultCol;
 vec4 textureCol;
+#define outline_width 0.001f
 
 vec4 effect( vec4 col, Image texture, vec2 texturePos, vec2 screenPos )
 {
-	number alpha = 4 * texture2D( texture, texturePos ).a;
-    alpha -= texture2D( texture, texturePos + vec2( 0.001f, 0.0f ) ).a;
-    alpha -= texture2D( texture, texturePos + vec2( -0.001f, 0.0f ) ).a;
-    alpha -= texture2D( texture, texturePos + vec2( 0.0f, 0.001f ) ).a;
-    alpha -= texture2D( texture, texturePos + vec2( 0.0f, -0.001f ) ).a;
+	number alpha = 8 * texture2D( texture, texturePos ).a;
+    alpha -= texture2D( texture, texturePos + vec2( outline_width, 0.0f ) ).a;
+    alpha -= texture2D( texture, texturePos + vec2( -outline_width, 0.0f ) ).a;
+    alpha -= texture2D( texture, texturePos + vec2( 0.0f, outline_width ) ).a;
+    alpha -= texture2D( texture, texturePos + vec2( 0.0f, -outline_width ) ).a;
+	alpha -= texture2D( texture, texturePos + vec2( -outline_width, -outline_width ) ).a;
+    alpha -= texture2D( texture, texturePos + vec2( outline_width, outline_width ) ).a;
+    alpha -= texture2D( texture, texturePos + vec2( -outline_width, outline_width ) ).a;
+    alpha -= texture2D( texture, texturePos + vec2( outline_width, -outline_width ) ).a;
 
 	// calculate resulting color
     return vec4( 1.0f, 1.0f, 1.0f, alpha );
